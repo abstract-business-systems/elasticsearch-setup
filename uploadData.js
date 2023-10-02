7ea44d49bd1917b23f15890e6aa0a46bc5988f77
nkg-abs <nkg@abs.in>
2023-10-02 12:15:44 +0530

    Restructured.

--- uploadData.js
+++ uploadData.js
@@ -1,20 +1,18 @@
 const { mapAsync } = require("./utils");
-const data = require("./data.json");
+const products = require("./products.json");
 
 const bulkUpdate = async ({ client }) => {
-  await mapAsync(data, async (coll, indexName) => {
     try {
-        mapAsync(coll, async (data) => {
+      await mapAsync(products, async (product) => {
             await client.index({
-                index: indexName,
-                body: data,
+                index: 'products',
+                body: product,
             });
             console.log('posted');
         })
     } catch (error) {
         console.error(`Error posting products: ${error}`);
     }
-  })
 };
 
 module.exports = bulkUpdate;
