19344b926d53f8a205009dc610aeba6ce7f1730a
nkg-abs <nkg@abs.in>
2023-10-09 17:04:52 +0530

    Dusted.

--- setup.sh
+++ setup.sh
@@ -6,9 +6,7 @@ source ./lib.sh
 fileContent=$(cat .data.json)
 node postData.js
 docker stop es01
-docker stop logstash-container
 docker rm es01
-docker rm logstash-container
 docker stop log-rdbms && docker rm log-rdbms
 docker run -u root -v /home/nkg/dev/es/data:/bitnami/elasticsearch/data:rw --name es01 --network host -d bitnami/elasticsearch:latest
 executeUntillSucceeds curl -X PUT localhost:9200/_cluster/settings?pretty -H "Content-Type: application/json" -d "$fileContent"
