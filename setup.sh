a21bd0a859adc083b2935cf6e7fb08c46258a6b7
nkg-abs <nkg@abs.in>
2023-10-10 16:31:04 +0530

    Moved absolute to relative path.

--- setup.sh
+++ setup.sh
@@ -7,6 +7,6 @@ fileContent=$(cat .data.json)
 node postData.js
 docker stop es01 && docker rm es01
 docker stop log-rdbms && docker rm log-rdbms
-docker run -u root -v /home/nkg/dev/es/temp:/bitnami/elasticsearch/data:rw --name es01 --network host -d bitnami/elasticsearch:latest
+docker run -u root -v ./storage/data:/bitnami/elasticsearch/storage/data:rw --name es01 --network host -d bitnami/elasticsearch:latest
 executeUntillSucceeds curl -X PUT localhost:9200/_cluster/settings?pretty -H "Content-Type: application/json" -d "$fileContent"
-docker run --env-file /home/nkg/dev/docker/logstash-db/env.txt --name log-rdbms --network host -v /home/nkg/dev/es/elasticsearch-config.yml:/usr/share/logstash/pipeline/logstash.conf -v /home/nkg/dev/docker/logstash-db/config/env.sh:/usr/share/logstash/vendor/jdbc/env.sh -d log-rdbms
+docker run --env-file ./.env --name log-rdbms --network host -v ./elasticsearch-config.yml:/usr/share/logstash/pipeline/logstash.conf -d log-rdbms
