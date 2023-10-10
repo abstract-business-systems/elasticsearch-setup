f7a96709e29c4a71c54f1cbca1bcd99bab45f5f2
nkg-abs <nkg@abs.in>
2023-10-10 16:39:40 +0530

    Refactored.

--- setup.sh
+++ setup.sh
@@ -3,10 +3,23 @@
 cd $(dirname "$0") 
 source ./lib.sh
 
-fileContent=$(cat .data.json)
-node postData.js
-docker stop es01 && docker rm es01
-docker stop log-rdbms && docker rm log-rdbms
-docker run -u root -v ./storage/data:/bitnami/elasticsearch/storage/data:rw --name es01 --network host -d bitnami/elasticsearch:latest
-executeUntillSucceeds curl -X PUT localhost:9200/_cluster/settings?pretty -H "Content-Type: application/json" -d "$fileContent"
-docker run --env-file ./.env --name log-rdbms --network host -v ./elasticsearch-config.yml:/usr/share/logstash/pipeline/logstash.conf -d log-rdbms
+populateDataToDb() {
+    node postData.js
+}
+haltExistingContainers() {
+    docker stop es01 && docker rm es01
+    docker stop log-rdbms && docker rm log-rdbms
+}
+setupElasticSearch() {
+    docker run -u root -v ./storage/data:/bitnami/elasticsearch/storage/data:rw --name es01 --network host -d bitnami/elasticsearch:latest
+    fileContent=$(cat esStorageConfig.json)
+    executeUntillSucceeds curl -X PUT localhost:9200/_cluster/settings?pretty -H "Content-Type: application/json" -d "$fileContent"
+}
+setupLogstash(){
+    docker run --env-file ./.env --name log-rdbms --network host -v ./elasticsearch-config.yml:/usr/share/logstash/pipeline/logstash.conf -d log-rdbms
+}
+
+populateDataToDb
+haltExistingContainers
+setupElasticSearch
+setupLogstash
