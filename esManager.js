ae1075b5fbf7fb0176eb476b9684326cd556f5a5
nkg-abs <nkg@abs.in>
2023-10-10 16:50:42 +0530

    Restructed.

---
+++ esManager.js
@@ -0,0 +1,50 @@
+const { Client } = require('@elastic/elasticsearch');
+const { mapAsync } = require("./utils");
+const products = require("./products.json");
+
+const bulkUpdate = async ({ client }) => {
+    try {
+      await mapAsync(products, async (product) => {
+            await client.index({
+                index: 'products',
+                body: product,
+            });
+            console.log('posted');
+        })
+    } catch (error) {
+        console.error(`Error posting products: ${error}`);
+    }
+};
+
+const createIndex = async ({ client, schema }) => {
+    await mapAsync(schema, async (props, indexName) => {
+        try {
+            const response = await client.indices.create({
+                index: indexName,
+                body: {
+                    settings: {
+                      number_of_shards: 1,
+                      number_of_replicas: 1
+                    },
+                    mappings: {
+                        properties: props,
+                    },
+                  },
+            });
+            console.log(`Index created: ${indexName}`);
+            console.log(response);
+          } catch (error) {
+            console.error(`Error creating index: ${error}`);
+          }
+    });
+};
+
+const connectToEs = (url) => new Client({
+    node: url,
+  });
+
+module.exports = {
+    bulkUpdate,
+    createIndex,
+    connectToEs,
+};
--- index.js
+++ index.js
@@ -1,18 +1,13 @@
-const { Client } = require('@elastic/elasticsearch');
 const schema = require('./schema');
-const createIndex = require('./createIndex');
-const bulkUpdate = require('./uploadData');
-
-const client = new Client({
-  node: 'http://localhost:9200', // Replace with your Elasticsearch cluster URL
-});
+const { connectToEs, createIndex, bulkUpdate } = require('./esManager');
 
 const main = async (context) => {
-  await createIndex(context);
-  await bulkUpdate(context);
+  const client = await connectToEs('http://localhost:9200');
+  const extendedContext = { ...context, client };
+  await createIndex(extendedContext);
+  await bulkUpdate(extendedContext);
 };
 
 main({
-  client,
   schema,
 });
