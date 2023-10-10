440eb42b5b0a95bf99cf00e9dd01acfa3aaa767a
nkg-abs <nkg@abs.in>
2023-10-10 16:54:26 +0530

    Introduced esManger.deleteIndex.

--- esManager.js
+++ esManager.js
@@ -1,50 +1,62 @@
-const { Client } = require('@elastic/elasticsearch');
+const { Client } = require("@elastic/elasticsearch");
 const { mapAsync } = require("./utils");
 const products = require("./products.json");
 
 const bulkUpdate = async ({ client }) => {
-    try {
-      await mapAsync(products, async (product) => {
-            await client.index({
-                index: 'products',
-                body: product,
-            });
-            console.log('posted');
-        })
-    } catch (error) {
-        console.error(`Error posting products: ${error}`);
-    }
+  try {
+    await mapAsync(products, async (product) => {
+      await client.index({
+        index: "products",
+        body: product,
+      });
+      console.log("posted");
+    });
+  } catch (error) {
+    console.error(`Error posting products: ${error}`);
+  }
 };
 
 const createIndex = async ({ client, schema }) => {
-    await mapAsync(schema, async (props, indexName) => {
-        try {
-            const response = await client.indices.create({
-                index: indexName,
-                body: {
-                    settings: {
-                      number_of_shards: 1,
-                      number_of_replicas: 1
-                    },
-                    mappings: {
-                        properties: props,
-                    },
-                  },
-            });
-            console.log(`Index created: ${indexName}`);
-            console.log(response);
-          } catch (error) {
-            console.error(`Error creating index: ${error}`);
-          }
-    });
+  await mapAsync(schema, async (props, indexName) => {
+    try {
+      const response = await client.indices.create({
+        index: indexName,
+        body: {
+          settings: {
+            number_of_shards: 1,
+            number_of_replicas: 1,
+          },
+          mappings: {
+            properties: props,
+          },
+        },
+      });
+      console.log(`Index created: ${indexName}`);
+      console.log(response);
+    } catch (error) {
+      console.error(`Error creating index: ${error}`);
+    }
+  });
 };
 
-const connectToEs = (url) => new Client({
+const connectToEs = (url) =>
+  new Client({
     node: url,
   });
 
+const deleteIndex = async ({ client, indexName }) => {
+  try {
+    const response = await client.indices.delete({ index: indexName });
+    console.log(`Index deleted: ${indexName}`);
+    console.log(response);
+  } catch (error) {
+    console.error(`Error deleting index: ${error}`);
+  }
+};
+
 module.exports = {
-    bulkUpdate,
-    createIndex,
-    connectToEs,
+  bulkUpdate,
+  createIndex,
+  connectToEs,
+  deleteIndex,
 };
